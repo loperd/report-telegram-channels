3aa5d662a10aa4d455387bf957b9559e8d2a8cbb
Roman <bizrenay@gmail.com>
2022-03-04 23:32:09 +0200

    add new dirty channels

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 import sys
 import os
 import random
@@ -23,6 +24,39 @@ def get_dirty_channels(filepath) -> list:
 def get_messages() -> list:
     with open(os.path.join(os.path.dirname(os.path.relpath(__file__)), 'messages.txt'), 'r') as fp:
         return fp.readlines()
+
+
+def get_remote_md5_sum(url):
+    return hash_remote_file(urlopen(url), "md5")
+
+
+def hash_remote_file(remote, algorithm="md5"):
+    max_file_size = 100 * 1024 * 1024
+
+    if algorithm == "md5":
+        hash_type = hashlib.md5()
+    elif algorithm == "sha1":
+        hash_type = hashlib.sha1()
+    elif algorithm == "sha256":
+        hash_type = hashlib.sha256()
+    elif algorithm == "sha384":
+        hash_type = hashlib.sha384()
+    elif algorithm == "sha512":
+        hash_type = hashlib.sha512()
+    else:
+        raise Exception("Hash type '{}' not found.".format(algorithm))
+
+    total_read = 0
+    while True:
+        data = remote.read(4096)
+        total_read += 4096
+
+        if not data or total_read > max_file_size:
+            break
+
+        hash_type.update(data)
+
+    return hash_type.hexdigest()
 
 
 def report_dirty_channel(client, channel, message):
@@ -55,6 +89,7 @@ def report_dirty_channels(api_id, api_hash):
             for dirty_channel in dirty_channels:
                 dirty_channel = dirty_channel.strip()
                 if dirty_channel in result_file_content:
+                    print("Skipped the channel {} because it has already reported it.".format(dirty_channel))
                     continue
                 msg = random.choice(messages).strip()
                 if not report_dirty_channel(tg_client, dirty_channel, msg):
@@ -76,9 +111,8 @@ if __name__ == '__main__':
 
     app_api_id = int(sys.argv[1])
     app_api_hash = sys.argv[2]
-    dirty_channels_filepath = sys.argv[3]
+    dirty_channels_file_url = sys.argv[3]
 
-    dirty_channels = get_dirty_channels(dirty_channels_filepath)
     messages = get_messages()
 
     result_file_path = os.path.join(
@@ -90,8 +124,21 @@ if __name__ == '__main__':
     if not os.path.exists(result_file_directory):
         os.makedirs(result_file_directory)
 
+    latest_hash_sum = ''
+    wait_secs = 30
+
     try:
-        report_dirty_channels(api_id=app_api_id, api_hash=app_api_hash)
+        while True:
+            dirty_channels = get_dirty_channels(dirty_channels_file_url)
+            new_hash_sum = get_remote_md5_sum(dirty_channels_file_url)
+
+            if new_hash_sum == latest_hash_sum:
+                print("Wait {} seconds for next updates.".format(wait_secs))
+                time.sleep(wait_secs)
+                continue
+
+            latest_hash_sum = new_hash_sum
+
+            report_dirty_channels(api_id=app_api_id, api_hash=app_api_hash)
     except KeyboardInterrupt:
         print('Closed..')
-
